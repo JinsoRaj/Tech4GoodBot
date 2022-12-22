5aa96e0ca509b755c28ff95b9cde6e8fd9301dfb
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2022-12-22 09:51:06 +0530

    Rss Feeds, Search Discourse inline.

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
 /*
   Project: Tech4Good Telegram Bot - t.me/JinsoRaj
-  Langauge: Javascript in Deno + Deno-Deploy
+  Langauge: Node.js
   Framework: grammYjs
   configs: https://t.me/c/1191474847/4711
 */
-import { Bot } from"grammy";
+import { Bot, InlineKeyboard, GrammyError, HttpError } from"grammy";
 import { checkNewDiscourse } from "./helpers/newEntries.js";
+import { getDiscoursePosts } from "./helpers/searchDiscourse.js"
 import cron from "node-cron";
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -13,11 +14,11 @@ export const bot = new Bot(process.env.BOT_TOKEN);
 
 // Handle the /start command.
 bot.command("start", (ctx) => {
-  ctx.reply("Yeah! Online...")
+  ctx.reply("Hi..\nI will update you with latest post from T4GLabs\nYou can also search the posts via inLine.")
 });
 
 // Check for New Discourse group RSS feeds every 10 mints.
-cron.schedule('*/15 * * * *', async () => {
+cron.schedule('*/10 * * * *', async () => {
   console.log('running a task every 10 minutes');
   await checkNewDiscourse()
 });
@@ -27,8 +28,36 @@ cron.schedule('*/15 * * * *', async () => {
 //await checkNewWebsite();
 //await checkNewLinkedin();
 
-// Inline
+// Inline Mode
 
+// Listen for users typing “@Tech4GoodBot ”
+bot.on('inline_query', async(ctx) => {
+  const query = ctx.inlineQuery.query;
+  const res = await getDiscoursePosts()
+  const resarr = res.filter(({ ["title"]: title }) => title && title.includes(query));
+  //console.log(resarr);
+  let data = resarr.map((item, index) => {
+    return {
+      type: "article",
+      id: String(index),
+      title: item.title,
+      input_message_content: {
+        message_text: `<b>${item.title}</b>\n<i>${item.description}</i>`,
+        parse_mode: "HTML",
+      },
+      reply_markup: new InlineKeyboard().url(
+        "Read Post",
+        item.link,
+      ),
+      url: item.link,
+      description: item.description,
+    }
+  })
+  await ctx.answerInlineQuery(
+    data,
+    { cache_time: 30 * 24 * 3600 }, // one month in seconds
+  );
+});
 
 bot.catch((err) => {
   const ctx = err.ctx;
--- helpers/searchDiscourse.js
+++ helpers/searchDiscourse.js
@@ -1,3 +1,33 @@
+import { read } from '@extractus/feed-extractor'
+import * as dotenv from "dotenv"
+dotenv.config()
 
-// Todo: cheerio?, /search?q=word%20in%3Atitle
-export async function getDiscoursePosts(){}
+const discourseUrl = process.env.DISCOURSE_RSS_URL
+
+// Todo: puppeteer?, /search?q=word%20in%3Atitle
+export async function getDiscoursePosts(){
+   try{
+      const result = await read(discourseUrl,{
+        getExtraFeedFields: (feedData) => {
+          return {
+            lastBuildDate: feedData.lastBuildDate || ''
+          }
+        },
+        getExtraEntryFields: (feedEntry) => {
+            const {
+              pubDate,
+              guid
+            } = feedEntry
+            return {
+              pubDate: pubDate,
+              guid: Number(guid["#text"].split('-')[2])
+            }
+          }
+        })
+      return result.entries;
+  
+    }catch(error){
+      console.log(`Error in fetching Discourse url: ${error}`);
+    }
+}
+//await getDiscoursePosts()
